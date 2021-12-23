5a0b26a3015c8d023b0e2c734d0428e5abc154ef
U170709023 <haticeozgulbilici@posta.mu.edu.tr>
2021-12-24 01:58:01 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/reminderapp/AddReminderActivity.java
@@ -0,0 +1,14 @@
+package tr.edu.mu.ceng.mad.reminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}

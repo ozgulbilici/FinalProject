2ece6e4ac6157db245b86419038eb197c015bbe2
U170709023 <haticeozgulbilici@posta.mu.edu.tr>
2021-12-25 05:42:15 +0300

    Added button in MainActivity,change button background colour...

--- app/src/main/java/tr/edu/mu/ceng/mad/reminderapp/MainActivity.java
+++ app/src/main/java/tr/edu/mu/ceng/mad/reminderapp/MainActivity.java
@@ -2,13 +2,44 @@ package tr.edu.mu.ceng.mad.reminderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btn1;
+    public Button btn2;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create buttons to transfer login and register page.
+        Button btn1 = (Button) findViewById(R.id.goToLogin);
+        Button btn2 = (Button) findViewById(R.id.goToSignUp);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }

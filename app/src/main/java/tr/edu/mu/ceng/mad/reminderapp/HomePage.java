80fe1cbeedae460cbdc7c7fd61b0cdb28e00ceb7
U170709023 <iremuslu03@gmail.com>
2021-12-27 00:09:28 +0300

    onBackPressed Method created
    created onBackPressed method.

--- app/src/main/java/tr/edu/mu/ceng/mad/reminderapp/HomePage.java
+++ app/src/main/java/tr/edu/mu/ceng/mad/reminderapp/HomePage.java
@@ -1,16 +1,23 @@
 package tr.edu.mu.ceng.mad.reminderapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.media.tv.TvContract;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity{ //implements NavigationView.OnNavigationItemSelectedListener{
 
     public NavigationView navigationView;
     public DrawerLayout drawer;
@@ -36,5 +43,49 @@ public class HomePage extends AppCompatActivity {
                 ,0,0);
         drawer.addDrawerListener(toogle);
         toogle.syncState();
+
+       // navigationView.setNavigationItemSelectedListener(this); ////Triggers the NavigationView.OnNavigationItemSelectedListener interface.
     }
+
+    /*
+    //This method tells which item was clicked.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.nav_profile){
+            Toast.makeText(getApplicationContext(),"Profile",Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.nav_reminders){
+            Toast.makeText(getApplicationContext(),"Reminders",Toast.LENGTH_SHORT).show();
+
+        }
+        if(item.getItemId() == R.id.nav_add_new_reminder){
+            Toast.makeText(getApplicationContext(),"Add New Reminder",Toast.LENGTH_SHORT).show();
+
+        }
+        if(item.getItemId() == R.id.nav_log_out){
+            Toast.makeText(getApplicationContext(),"Log out",Toast.LENGTH_SHORT).show();
+            fragment = new LogOutFragment();
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_keep,fragment).commit();
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+    } */
+
+    public void onBackPressed(){
+
+        if(drawer.isDrawerOpen(GravityCompat.START)){ //If the Drawer is open, pressing the back button will first close the navigation drawer.
+            drawer.closeDrawer(GravityCompat.START);
+        }
+
+        else{ //If the drawer is closed, the application exits.
+            Intent myIntent = new Intent(Intent.ACTION_MAIN);
+            myIntent.addCategory(Intent.CATEGORY_HOME);
+            myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(myIntent);
+        }
+    }
+
+
 }
